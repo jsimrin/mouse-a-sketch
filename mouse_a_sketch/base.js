757d6934f4451bcda269590894ddfe0a786f4490
jsimrin <jsimrin@gmail.com>
2015-05-07 20:40:10 -0700

    CommitToMaster
    here’s my solution

--- mouse_a_sketch/base.js
+++ mouse_a_sketch/base.js
@@ -2,4 +2,12 @@ console.log("this is a sanity check");
 
 $(document).ready(function(){
   console.log("Let's get coding!")
+
+	function colorMe() {
+		$(this).css("background", $("input").val());
+	}
+
+	$("td").mouseover(colorMe)
+
 })
+
